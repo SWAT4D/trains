37bbce2c79e99413aa7f7363f4cb9d3a5a08ae24
attilamarx <atimarx@gmail.com>
2017-03-18 14:37:18 +0100

    Update EntryPoint.java

--- src/EntryPoint.java
+++ src/EntryPoint.java
@@ -10,6 +10,9 @@ public class EntryPoint extends Rail {
      * Default constructor
      */
     public EntryPoint() {
+        Logger.logStart("EntryPoint() - EntryPoint");
+        
+        Logger.logEnd();
     }
 
 }
