407665d3be875f401984215cff0e5be077a68046
Hegyi <hegyia02@gmail.com>
2017-03-18 10:25:53 +0100

    Fixed Comments

--- src/Logger.java
+++ src/Logger.java
@@ -1,15 +1,15 @@
-/*
-Logger oszály
-
-Statikus osztály csak statikus tagjai vannak, ezért könnyen meg lehet
-hívni a pl: Logger.write() függvényt kiíráshoz.
-
-A használata:
-    + A használat előtt az initInc()-nek le kell futni
-    + A kiíráshoz a write() függvényt lehet használni
-            (Ami már a beállított tabulátorokkal ír ki)
-    + Függvényhívások előtt az inc()-et kell meghívni
-    + Függvényhívások után a dec()-t.
+/**
+ * Logger oszály
+ *
+ * Statikus osztály csak statikus tagjai vannak, ezért könnyen meg lehet
+ * hívni a pl: Logger.write() függvényt kiíráshoz.
+ *
+ * A használata:
+ *     + A használat előtt az initInc()-nek le kell futni
+ *     + A kiíráshoz a write() függvényt lehet használni
+ *             (Ami már a beállított tabulátorokkal ír ki)
+ *     + Függvényhívások előtt az inc()-et kell meghívni
+ *     + Függvényhívások után a dec()-t.
  */
 
 public class Logger {
@@ -33,18 +33,25 @@ public class Logger {
 
     }
 
-    //A logger osztály használata előtt kell
-    // ennek le kell futni a main-ben
+
+    /**
+     * A logger osztály használata előtt kell
+     * ennek le kell futni a main-ben
+     */
     static void initInc(){
         tab = 0;
     }
 
-    //Növeli a tabulátorok számát
+    /**
+     * Növeli a tabulátorok számát
+     */
     static void inc(){
         tab++;
     }
 
-    //Csökkenti a tabulátorok számát
+    /**
+     * Csökkenti a tabulátorok számát
+     */
     static void dec(){
         tab--;
     }
