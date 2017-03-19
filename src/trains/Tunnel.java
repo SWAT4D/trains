1656f70296ddcb03c90c6a0a6aec1b49578f9819
Defearon <markusz996@gmail.com>
2017-03-19 23:45:01 +0100

    Update Tunnel.java

--- src/trains/Tunnel.java
+++ src/trains/Tunnel.java
@@ -9,7 +9,7 @@ public class Tunnel extends Rail {
 	/**
 	 * Currently active TPs
 	 */
-	private static int activeNum;
+	private static int activeNum = 0;
 	private static TunnelPlace first, sec;
 	private static Tunnel instance = null;
     
@@ -18,11 +18,13 @@ public class Tunnel extends Rail {
      */
     private Tunnel() {
     	Logger.logStart("The One-And-Only Tunnel was created.");
-    	start = null;
-    	end = null;
+    	/*
+    	 * TEMP! 
+    	 */
+    	start = new Rail();
+    	end = new Rail();
     	first = null;
     	sec = null;
-    	activeNum = 0;
     	Logger.logEnd();
     }
     
@@ -44,31 +46,34 @@ public class Tunnel extends Rail {
      * @param tunnelPlace
      */
     public static void activeTunnelPlace(TunnelPlace tunnelPlace) {
+    	Logger.logStart("activeTunnelPlace");
     	switch (activeNum){
 	    	case 0:
-	    		Logger.logStart("TP activated. There are 1 active.");
+	    		Logger.logMessage("TP activated. There are 1 active.");
 	    		first = tunnelPlace;
-	    		start.addPrev(first);
+	    		start.addPrev(tunnelPlace);
 	    		activeNum++;
 	    		Logger.logEnd();
+	    		first.setIsActive(true);
 	    		break;
 	    	case 1:
-	    		Logger.logStart("TP activated. There are 2 active.");
+	    		Logger.logMessage("TP activated. There are 2 active.");
 	    		sec = tunnelPlace;
 	    		end.addNext(sec);
 	    		first.addNext(start);
 	    		sec.addNext(end);
 	    		activeNum++;
+	    		sec.setIsActive(true);
 	    		Logger.logEnd();
 	    		break;
 	    	case 2:
-	    		Logger.logStart("There are 2 active TPs already!");
+	    		Logger.logMessage("There are 2 active TPs already!");
 	    		Logger.logEnd();
 	    		break;
 	    	default:
-	    		Logger.logStart("WTF! How did I get here?!");
+	    		Logger.logMessage("WTF! How did I get here?!");
         		Logger.logEnd();
-	    		break;
+        		break;
 	    		
     	}
     }
@@ -85,28 +90,26 @@ public class Tunnel extends Rail {
      * @param tunnelPlace
      */
     public static void inactiveTunnelPlace(TunnelPlace tunnelPlace ) {
-        switch (activeNum){
+    	Logger.logStart("inactiveTunnelPlace");
+    	switch (activeNum){
         	case 1:
-        		Logger.logStart("TP inactivated. There are 0 active.");
+        		Logger.logMessage("TP inactivated. There are 0 active.");
         		start.addPrev(null);
-	    		first = null;
 	    		activeNum--;
-			Logger.logEnd();
         		break;
         	case 2:
-        		
-        		Logger.logStart("TPs was inactivated, Tunnel was destroyed.");
+        		Logger.logMessage("TPs was inactivated, Tunnel was destroyed.");
+        		first.setIsActive(false);
+        		sec.setIsActive(false);
         		end.addNext(null);
         		start.addPrev(null);
 	    		first.addNext(null);
 	    		sec.addNext(null);
-	    		first = null;
-	    		sec = null;
 	    		activeNum = 0;
 	    		Logger.logEnd();
         		break;
         	default:
-        		Logger.logStart("WTF! How did I get here?!");
+        		Logger.logMessage("WTF! How did I get here?!");
         		Logger.logEnd();
         		break;
         }
