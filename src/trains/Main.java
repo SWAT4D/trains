b895e361aaaaa5cc048c6f03e4c59c176bbdcfaf
attilamarx <atimarx@gmail.com>
2017-03-20 00:20:27 +0100

    Update Main.java

--- src/trains/Main.java
+++ src/trains/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static boolean play;
+    public static boolean isoccupied = false;
     public static void menu1(){
         System.out.println("  _______        _                 _        _      _              	");
         System.out.println(" |__   __|      (_)               | |      | |    | |             	");
@@ -16,15 +17,19 @@ public class Main {
         System.out.println("|------------------------------------------------------------------|	");
         System.out.println("|    1. Vakvágányra érkezés                                        |	");
         System.out.println("|    2. Vonat mozgása ütközés detektálással                        |	");
-        System.out.println("|    3. Kocsi elhagyja a pályát                                    |	");
-        System.out.println("|    4. Alagút szimulálása                                         |	");
-        System.out.println("|    5. Kilépés                                                    |	");
+        System.out.println("|    3. Kocsi utassal hagyja el a pályát                           |	");
+        System.out.println("|    4. Kocsi állomásra léptetése                                  |	");
+        System.out.println("|    5. Váltó váltása                                              |	");
+        System.out.println("|    6. Alagút szimulálása                                         |	");
+        System.out.println("|    7. Kilépés                                                    |	");
+        System.out.println("|                                                                  |	");
         System.out.println("|                                                                  |	");
         System.out.println("+------------------------------------------------------------------+	");
     }
-    
+
     public static void main (String[] asrg){
-        int select = 0;
+        Logger.init();
+        int select=0;
         do {
             play = true;
             menu1();
@@ -48,40 +53,32 @@ public class Main {
                     scanner.nextLine(); // Discard '\n'
                     break;
                 case 4:
-                	BUILD_TUNNEL();
-                	Logger.logMessage("Menübe való visszatéréshez nyomj entert");
+                    STEP_TO_STATION();
+                    Logger.logMessage("Menübe való visszatéréshez nyomj entert");
                     scanner.nextLine(); // Discard '\n'
                     break;
-                	
+                    
+                case 5:
+                    SWITCH_THE_SWITCH();
+                    Logger.logMessage("Menübe való visszatéréshez nyomj entert");
+                    scanner.nextLine(); // Discard '\n'
+                    break;
+                case 6:
+                    BUILD_TUNNEL();
+                    Logger.logMessage("Menübe való visszatéréshez nyomj entert");
+                    scanner.nextLine(); // Discard '\n'
+                    break;
             }
 
-        } while (select != 5);
+        } while (select != 7);
 
     }
-    static void BUILD_TUNNEL(){
-    	
-    	/**
-         * Pályaelemek létrehozása a teszthez
-         * EndVoid -> EntryPoint -> null
-         */
-    	EndVoid ev = new EndVoid();
-    	EntryPoint ep = new EntryPoint();
-    	ep.addPrev(ev);
-    	Tunnel tt = Tunnel.getInstance();  
-    	
-    	TunnelPlace t1 = new TunnelPlace();
-    	t1.setActive();
-    	
-    	TunnelPlace t2 = new TunnelPlace();
-    	t2.setActive();
-    	
-    	TunnelPlace t3 = new TunnelPlace();
-    	t3.setActive();
-    	
-    	t2.setActive();
 
-    }
+    
     static void STEP_TO_DEAD_END (){
+
+        Logger.off();
+
         /**
          * Pályaelemek létrehozása a teszthez
          * EndVoid -> EntryPoint -> null
@@ -99,38 +96,60 @@ public class Main {
         Car c = new Car(ev);
         l.addNext(c);
         l.setStartPlace(ep);
+
+        Logger.on();
 
         l.step();
     }
 
     static void OCCUPIED_RAIL(){
+
+        Logger.off();
+
         /**
-         * Pályaelemek létrehozása a teszthez
-         * EndVoid -> EntryPoint -> EntryPoint -> EndVoid
+         * Pályaelemek létrehozása a teszthez alapeset:
+         * EndVoid -> EntryPoint -> Rail
          */
         EndVoid ev = new EndVoid();
-        EntryPoint ep = new EntryPoint();
-        EntryPoint ep2 = new EntryPoint();
+        EntryPoint ep1 = new EntryPoint();
+        ep1.addPrev(ev);
+        Rail nextRail = new Rail(); //egy sín létrehozása
+        ep1.addNext(nextRail);
 
-        ep.addPrev(ev);
-        ep.addNext(ep2);
-        ep2.addNext(ev);
-
+        /**
+         * Teszt vizsgálata, ha nincs ütközés:
+         * EndVoid -> EntryPoint -> Rail -> EntryPoint -> EndVoid
+         *
+         * A loggolva jelezzük hogy milyen opciót választott a felhasználó
+         */
+        Logger.on();
+        Logger.logMessage("Van ütközés?(i/n)");
+        Scanner sc = new Scanner(System.in);
+        String ans = sc.next();
+        if(ans.equals("i")){
+            Main.isoccupied = true;
+        }
+        Logger.logMessage("A választott opció: " + ans);
+        
+        Logger.off();
         /**
          * Egy mozdony és hozzá egy kocsi
          *  - létrehozás és összekötés
          *  - mozdony kezdőpontra helyezése
          */
-        Locomotive l = new Locomotive(ev);
-        Car c = new Car(ev);
-        l.addNext(c);
-        l.setStartPlace(ep);
+        Locomotive l1 = new Locomotive(ev);
+        Car c1 = new Car(ev);
+        l1.addNext(c1);
+        l1.setStartPlace(ep1);
 
-        l.step();
+        Logger.on();
 
+        l1.step();
+        Main.isoccupied = false;    //hogy ne lehessen true véletlen
     }
 
     static void REMAIN_PASSENGER(){
+        Logger.off();
         /**
          * Pályaelemek létrehozása a teszthez
          * EndVoid -> EntryPoint -> EndVoid
@@ -149,7 +168,7 @@ public class Main {
         Car c = new Car(ev);
         l.addNext(c);
         l.setStartPlace(ep);
-
+        Logger.on();
         /**
          * Indítás. A futás menete:
          *  1. Mozdony rálép az entrypointról az endvoid-ra
@@ -158,5 +177,92 @@ public class Main {
          */
         l.step();
     }
+    
+    static void STEP_TO_STATION(){
+
+        Logger.off();
+        /**
+        * Pályaelemek létrehozása a teszthez
+        * Rail -> Station -> Rail
+        */
+        Rail r1 = new Rail();   //egy sín létrehozása
+        Rail r2 = new Rail();
+        Station st1 = new Station("valamilyenSzin"); //állomás létrehozása
+        Rail r3 = new Rail(); 
+        r2.addNext(r2);
+        r2.addNext(st1);
+        st1.addNext(r3);
+        
+        Locomotive l1 = new Locomotive(st1, r2); //mozdony létrehozása teszthez
+        Car c1 = new Car(r2);
+        Car c2 = new Car(r1);
+        l1.addNext(c1);
+        c1.addNext(c2);
+
+        Logger.on();
+
+        l1.step();
+        
+    }
+                        
+    static void SWITCH_THE_SWITCH()
+    {
+        Logger.off();
+        /*
+        *Pálya elemek létrehozása a teszthez
+        *Rail -> Switch -> Rail
+        *               -> Rail
+        */
+        Rail main = new Rail();   //sínek létrehozása
+        Rail alt = new Rail();
+        Rail prev = new Rail();
+        Switch sw = new Switch(); //váltó létrehozása
+        prev.addNext(sw);//sínek összekötése
+        sw.addNext(main);
+        sw.addNextAlt(alt);
+        
+        Locomotive l = new Locomotive(sw, prev);//mozdony a teszthez
+        Car c = new Car(prev);
+        l.addNext(c);
+        
+        Logger.on();
+        /*
+        *A váltó az alaphelyzetből a mellékágra vált.
+        *A vonat áthalad a váltón
+        */
+        
+        Logger.logMessage("Átakarod váltani a váltót?(i/n)");
+        Scanner sc = new Scanner(System.in);
+        String ans = sc.next();
+        if(ans.equals("i"))
+            sw.switchIt();
+        l.step();
+    }
+    
+    static void BUILD_TUNNEL(){
+    	
+    	/**
+         * Pályaelemek létrehozása a teszthez
+         * EndVoid -> EntryPoint -> null
+         */
+        Logger.off();
+        
+    	EndVoid ev = new EndVoid();
+    	EntryPoint ep = new EntryPoint();
+    	ep.addPrev(ev);
+    	Tunnel tt = Tunnel.getInstance();  
+    	TunnelPlace t1 = new TunnelPlace(); //create TunnelPlaces
+        TunnelPlace t2 = new TunnelPlace();
+        TunnelPlace t3 = new TunnelPlace();
+        
+        Logger.on();
+        
+    	t1.setActive();
+    	t2.setActive();
+    	t3.setActive();
+    	t2.setActive();
+
+    }
+    
 }
 
