e043437130daa77b3761ffb7c354c163f83293a1
Barbara Lócsi <locsi.barbara@gmail.com>
2017-04-22 17:00:35 +0200

    Make locos and cars round.

--- src/trains/GamePanel.java
+++ src/trains/GamePanel.java
@@ -33,24 +33,27 @@ public class GamePanel extends JPanel{
         for(int i=0; i<10; i++) {
             for (int j = 0; j < 10; j++) {
                 Koo searched = new Koo(j, i);
-                String s = ".";
-                String sc = "";
+                String sMap = ".";
+                String scMap = "";
+
+                String sTrain= ".";
+                boolean isTrain = false;
                 for (Map.Entry<Koo, Rail> entry : map.entrySet()) {
                     if (entry.getKey().compareTo(searched) == 0) {
                         if (entry.getValue().getTrain() != null) {
-                            s = entry.getValue().getTrain().toString();
-                        } else {
-                            s = entry.getValue().toString();
-                            sc = entry.getValue().getColor();
+                            sTrain = entry.getValue().getTrain().toString();
+                            isTrain = true;
                         }
+                        sMap = entry.getValue().toString();
+                        scMap = entry.getValue().getColor();
                     }
                 }
                 Color c = Color.WHITE;
-                switch(s){
+                switch(sMap){
                     case "E":
                         c = new Color(153,76,0); break;
                     case "S":
-                        switch(sc){
+                        switch(scMap){
                             case "r":
                                 c = new Color(255, 153, 153); break;
                             case "b":
@@ -60,12 +63,10 @@ public class GamePanel extends JPanel{
                             default:
                                 c = Color.MAGENTA; break;
                         }break;
-                    case "l":
-                        c = Color.BLACK; break;
                     case "+":
                         c = new Color(153,76,0); break;
                     case "G":
-                        switch(sc){
+                        switch(scMap){
                             case "r":
                                 c = new Color(204, 0, 0); break;
                             case "b":
@@ -77,20 +78,33 @@ public class GamePanel extends JPanel{
                         }break;
                     case "T":
                         c = new Color(128, 128, 128); break;
-                    case "r":
-                        c = new Color(255, 70, 70); break;
-                    case "b":
-                        c = new Color(70,160,255); break;
-                    case "g":
-                        c = new Color(70, 255, 70); break;
-                    case "e":
-                        c = Color.YELLOW; break;
                     default:
                         c = Color.WHITE; break;
+                }
+                Color cTrain = Color.WHITE;
+                if(isTrain) {
+                    switch (sTrain) {
+                        case "l":
+                            cTrain = Color.BLACK; break;
+                        case "r":
+                            cTrain = new Color(255, 70, 70); break;
+                        case "b":
+                            cTrain = new Color(70,160,255); break;
+                        case "g":
+                            cTrain = new Color(70, 255, 70); break;
+                        case "e":
+                            cTrain = Color.YELLOW; break;
+                        default:
+                            isTrain = false; break;
+                    }
                 }
 
                 g2.setColor(c);
                 g.fillRect(j*rectwidth, i*rectheight , rectwidth, rectheight);
+                if(isTrain){
+                    g2.setColor(cTrain);
+                    g.fillOval(j*rectwidth, i*rectheight , rectwidth, rectheight);
+                }
             }
         }
 
